d89043d9bc65bb07218c7e13aba31ab46a130a99
root <root@ip-172-31-47-220.ap-south-1.compute.internal>
2024-02-25 03:04:11 +0000

    This is my first commit

---
+++ file.sql
@@ -0,0 +1,14 @@
+select department_id,max(salary) from employees 
+group by department_id
+having department_id=60;
+
+declare
+Type yuva is record( A employees.department_id%type, B employees.salary%type);
+i yuva;
+begin
+select department_id,max(salary) into i from employees 
+group by department_id
+having department_id=60;
+dbms_output.put_line('The Maximum salary in department_'||i.A||'is'||i.B);
+end;
+/
